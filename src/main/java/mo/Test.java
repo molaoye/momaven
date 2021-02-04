fa8deaeeb283022c8d16ff775dd0a6aeec382f3e
moguoxing <mgxad@devitop.com>
2021-02-04 18:09:39 +0800

    修改

---
+++ src/main/java/mo/Test.java
@@ -0,0 +1,19 @@
+﻿package mo;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class Test {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("hello world!");
+    }
+
+}
